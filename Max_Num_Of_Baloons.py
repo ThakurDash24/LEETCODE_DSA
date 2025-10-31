a91bec9e9a84a1c71353b61a843c642cd78700f1
ThakurDash24 <thkrdash@gmail.com>
2025-10-31 22:33:10 +0530

    LEETCODE -1189

---
+++ Max_Num_Of_Baloons.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        b = text.count('b')
+        a = text.count('a')
+        l = text.count('l')//2
+        o = text.count('o')//2
+        n = text.count('n')
+        return min(b,a,l,o,n)
+        
+
+solver = Solution()
+text = "nlaebolko"
+output = solver.maxNumberOfBalloons(text)
+print(output)
